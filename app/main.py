e14955f097e46f5a97a090c601ec2463b3aeb908
Vitor Nascimento de Araujo <vnaraujo@lnxcit014027.cit>
2018-07-30 18:02:25 -0300

    adjust message id

--- app/main.py
+++ app/main.py
@@ -15,7 +15,8 @@ def container_create(name):
 @celery.task(exchange='lxc-client', name='{}.create_container_task'.format(__name__))
 def create_container_task(name):
     time.sleep(3)
-    return 'CONTAINER {} CREATED WITOUT ERRORS'.format(name)
+    return {'status':'CONTAINER {} CREATED WITOUT ERRORS'.format(name)}
+
 
 # celery -A app.main.celery worker --loglevel=info
 # docker run -d --hostname flask-rabbit --name flask-task-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
