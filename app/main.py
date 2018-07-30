8dc348043276dee4ea6759582b0d78cfe612e5c2
Vitor Nascimento de Araujo <vnaraujo@lnxcit014027.cit>
2018-07-30 10:19:18 -0300

    current task

--- app/main.py
+++ app/main.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 celery = make_celery(app)
 
 
-@app.route('/container/create/<name>', methods=['POST'])
+@app.route('/container/create/<name>', methods=['POST','GET'])
 def container_create(name):
-    create_container_task.delay(name)
+    task = create_container_task.delay(name)
     response = {
-        'status': 'CONTAINER {} CREATION IN PROGRESS...'.format(name)}
+        'status': 'CONTAINER {} CREATION IN PROGRESS... {}'.format(name, task.id)}
     return jsonify(response)
 
 
-@celery.task(name='{}.create_container_task'.format(__name__))
+@celery.task(exchange='lxc-client', name='{}.create_container_task'.format(__name__))
 def create_container_task(name):
-    print('PROCESSING')
     time.sleep(3)
     return 'CONTAINER {} CREATED WITOUT ERRORS'.format(name)
 
 # celery -A app.main.celery worker --loglevel=info
 # docker run -d --hostname flask-rabbit --name flask-task-rabbit -p 15672:15672 -p 5672:5672 rabbitmq:3-management
+# celery amqp
